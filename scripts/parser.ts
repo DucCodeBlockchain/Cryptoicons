d74e510691824768a5b321b48476809f0d738e5b
Vietvtc4 <146624254+DucCodeBlockchain@users.noreply.github.com>
2024-06-16 23:02:37 +0700

    ++

--- scripts/parser.ts
+++
@@ -1,9 +0,0 @@
-import { ElementNode, parse as svgParser } from 'svg-parser'
-
-/**
- * Creates AST object from SVG string
- */
-export function parse(svgString: string) {
-    const parsed = svgParser(svgString)
-    return parsed.children[0] as ElementNode
-}
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './components'
-export * from './createIcon'
-export * from './Cryptocon'
-export * from './CryptoconBadge'
-export * from './CryptoconLogo'
-export * from './Icon'
-export * from './types'
